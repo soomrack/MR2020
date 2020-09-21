ae845557eeb0fa77b346d853fc22f2440e5ed580
SailorTheMan <61163585+SailorTheMan@users.noreply.github.com>
2020-09-21 12:23:09 +0300

    Redrov's files added

---
+++ Redrov/assignment_1/main.c
@@ -0,0 +1,9 @@
+//
+// Created by vladl on 19.09.2020.
+//
+#include "stdio.h"
+
+
+int main(void){
+    printf("Hello world");
+}
---
+++ Redrov/assignment_2/main.h
@@ -0,0 +1,26 @@
+//
+// Created by vladl on 19.09.2020.
+//
+
+#include <stdio.h>
+
+#define NAME_QUESTION "What is your name?\n"
+#define AGE_QUESTION "How old are you?\n"
+#define START_QUESTION "Hi %s\nYour age is %d\nDo you want to play? (y/n)\n"
+#define READY_QUESTION "We gonna play true/false game\nI will tell you a statement and you should tell me\nwhether it is true(y) or false(n)\nSend me 'some char' to start the game\n"
+#define FIRST_QUESTION "Is it true that you are the best programmer ever?\n"
+#define SECOND_QUESTION "Is it true that c programming is easy?\n"
+#define THIRD_QUESTION "Is it true that this quiz is bad?\n"
+#define FOURTH_QUESTION "Is it true that two plus two is five?\n"
+#define FIFTH_QUESTION "Is the folowing statement correct? Kogda IMMiT? Vsegda IMMiT!\n"
+#define FIVE_OF_FIVE "Oh wow. That is 5/5. You are somehow answered the most difficult questions in the world without any mistakes\n"
+#define FOUR_OF_FIVE "4/5. Bro, it's really good result!\n"
+#define THREE_OF_FIVE "3/5. That's not your fault. Quiz is quite difficult, I know.\n"
+#define TWO_OF_FIVE "2/5. Maybe you should try again?\n"
+#define ONE_OF_FIVE "1/5. Appreciate that you took part of this quiz. Maybe another time?\n"
+#define ZERO_OF_FIVE "0/5. Smile. It was a prank. Camera is on the top of the screen\n"
+
+#ifndef QUIZ_MAIN_H
+#define QUIZ_MAIN_H
+
+#endif //QUIZ_MAIN_H
