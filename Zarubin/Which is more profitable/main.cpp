8f0b145fa0951c473c98539da0b979798a0c842f
zarubinKV <70758674+zarubinKV@users.noreply.github.com>
2020-09-21 13:50:43 +0300

    Add files via upload

---
+++ Zarubin/Which is more profitable/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int zp;
+    cout << "Enter the amount (penny) that you are ready to invest:" << endl;
+    cin >> zp;
+    int startkap;
+    cout << "Enter the starting capital (penny):" << endl;
+    cin >> startkap;
+    int periodIP;
+    cout << "Enter the term of the mortgage (months):" << endl;
+    cin >> periodIP;
+    int cost;
+    cout << "Enter the cost of the apartment (penny):" << endl;
+    cin >> cost;
+    int procIP;
+    cout << "Enter your mortgage percentage:" << endl;
+    cin >> procIP;
+    procIP=procIP/12;
+    int procVk;
+    cout << "Enter the percentage of the deposit:" << endl;
+    cin >> procVk;
+    int Rost;
+    cout << "Enter the rise in the cost of the apartment:" << endl;
+    cin >> Rost;
+    int ;
+    for(int i=0; i<=periodIP; i++){
+        
+    }
+    return 0;
+}
