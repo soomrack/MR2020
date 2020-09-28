ad326e9fcd7090b8b2ef5ea48e46faf01f89f52a
AlekseiLeonov <70850153+AlekseiLeonov@users.noreply.github.com>
2020-09-28 12:30:42 +0300

    Add files via upload

---
+++ Leonov/Leonov_Victorina.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <clocale>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char N;
+    char a;
+    char b;
+    char c;
+    char d;
+    int i;
+    setlocale(LC_CTYPE, "rus");
+    std::cout << "Здравствуй пользователь!\n";
+    std::cout << "Сейчас сыграем в викторину\n";
+     std::cout << "Вопрос 1: Самая высокая точка на земле\n";
+     std::cout << "Сейчас сыграем в викторину\n";
+     std::cout << "Сейчас сыграем в викторину\n";
+     std::cout << "Сейчас сыграем в викторину\n";
+
+    std::cin >> N;
+
+   
+}
