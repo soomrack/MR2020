6e92733541a9dd9c47eca656e83808792cded21d
miroshnichenko-danil <70773115+miroshnichenko-danil@users.noreply.github.com>
2020-10-19 12:10:48 +0300

    Add files via upload

---
+++ Miroshnichenko/TicTacToeOnline/ServerTicTacToe1.cpp
@@ -0,0 +1,52 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+#pragma warning(disable: 4996)
+
+char msg[9] = { '-','-','-','-','-','-','-','-','-' };
+
+int main(int argc, char* argv[]) {
+	//WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+	if (newConnection == 0) {
+		std::cout << "Error #2\n";
+	}
+	else {
+		std::cout << "Client Connected!\n";
+	}
+
+	while (true) {
+		recv(newConnection, msg, sizeof(msg), NULL);
+		for (int i = 0; i < 9; i++) {
+			if (msg[i] == '-') {
+				msg[i] = '0';
+				break;
+			}
+			
+		}
+		send(newConnection, msg, sizeof(msg), NULL);
+	}
+
+	system("pause");
+	return 0;
+}
