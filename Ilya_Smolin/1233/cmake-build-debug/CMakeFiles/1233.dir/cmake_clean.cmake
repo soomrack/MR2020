e1e3f35c409a876ccdf01f94d7cddbc4b9bc973c
Илья Смолин <70722225+Ilya-Smolin@users.noreply.github.com>
2020-10-27 17:29:05 +0300

    Add files via upload

---
+++ Ilya_Smolin/1233/cmake-build-debug/CMakeFiles/1233.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1233.exe"
+  "1233.exe.manifest"
+  "1233.pdb"
+  "CMakeFiles/1233.dir/main.c.obj"
+  "lib1233.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1233.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
