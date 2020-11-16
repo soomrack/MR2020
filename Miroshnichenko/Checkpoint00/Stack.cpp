be2cc291c58aeeec7201b289801b04d9fc3af440
miroshnichenko-danil <70773115+miroshnichenko-danil@users.noreply.github.com>
2020-11-16 14:41:59 +0300

    Stack

---
+++ Miroshnichenko/Checkpoint00/Stack.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+
+class Stack {
+public:
+  int pop();
+  void push(const int value);
+public:
+  Stack();
+  ~Stack();
+private:
+  struct Element
+  {
+    int value;
+    struct Element * next;
+  };
+  struct Element * top;
+};
+
+int Stack::pop() {
+  if (top == nullptr) {
+    std::cout << "ERROR: There is no elements in stack\n";
+    exit(1);
+  }
+  struct Element * pointer;
+  int a;
+  pointer = top; 
+  a = pointer->value; 
+  top = pointer->next; //элемент, следующий за вершиной стал вершиной стека
+  delete pointer;
+  return a; //возвращаем вершину стека
+}
+void Stack::push(const int value) {
+  struct Element * pointer;
+  pointer = new struct Element;
+  pointer->value = value;
+  pointer->next = top;
+  top = pointer;
+}
+
+Stack::Stack() {
+  top = nullptr;
+}
+
+Stack::~Stack() {
+  while (top != nullptr) {
+    pop();
+  }
+}
+
+int main() {
+  std::cout << "The end." << std::endl;
+}
