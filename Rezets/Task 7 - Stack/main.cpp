65ed74d1381032577aee440e95c271e987c77eff
rezetsua <70758824+rezetsua@users.noreply.github.com>
2020-11-16 14:57:17 +0300

    Add files via upload

--- Rezets/Task 7 - Stack/main.cpp
+++ Rezets/Task 7 - Stack/main.cpp
@@ -5,32 +5,40 @@
 class Stack
 {
 public:
-    int pop()
-    {
-        size--;
-        return data[size];
-    }
-    
-    void push(int value)
-    {
-        data[size] = value;
-        size++;
-    }
+    int pop();
+    void push(int value);
 public:
-    Stack()
-    {
-        size = 0;
-    }
-    
-    ~Stack()
-    {
-        while (size)
-            pop();
-    }
+    Stack();
+    ~Stack();
 private:
     int data[STACK_MAX_SIZE];
     size_t size;
 };
+
+int Stack::pop()
+{
+    if (size == 0)
+        return -101;
+    size--;
+    return data[size];
+}
+
+void Stack::push(int value)
+{
+    data[size] = value;
+    size++;
+}
+
+Stack::Stack()
+{
+    size = 0;
+}
+
+Stack::~Stack()
+{
+    while (size)
+    pop();
+}
 
 int main()
 {
