697a657a5e774ce22fd37d8b3ea835bcdc745036
Eugeniy <krai41@yandex.ru>
2020-11-16 15:02:33 +0300

    Add files via upload
    реализация стека

---
+++ Kraevskiy/checkpoint00/stack/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+class Stack {
+
+public:
+    int pop();
+    void push(const int value);
+    Stack();
+    ~Stack();
+    bool isEmpty();
+
+private:
+    struct node {
+        int d;
+        node *p;
+    };
+    node *top;
+};
+
+Stack::Stack() :top(NULL) {}
+
+void Stack::push(const int value)
+{
+    node *pv = new node;
+    pv->d = value;
+    pv->p = top;
+    top = pv;
+}
+
+int Stack::pop()
+{
+    if (isEmpty())
+    {
+        return -1;
+    }
+    int temp = top->d;
+    node *pv = top;
+    top = top->p;
+    delete pv;
+    return temp;
+}
+
+bool Stack::isEmpty()
+{
+    return top ? false : true;
+}
+
+
+Stack::~Stack()
+{
+    while (top)
+    {
+        node *pv = top;
+        top = top->p;
+        delete pv;
+    }
+}
+
+void emptyStack(Stack & stack);
+
+int main()
+{
+    std::cout << "The end." << std::endl;
+}
