63430a29a1c5b8bda692f488d4ec9468d3b2b59d
Serov_danila <70762407+SerovDanila@users.noreply.github.com>
2020-11-29 19:40:23 +0300

    Поправил дерево, теперь оно вроде даже работает

--- Serov/checkpoint/Tree/main.cpp
+++ Serov/checkpoint/Tree/main.cpp
@@ -12,7 +12,7 @@ public:
     Node *left;
     Node *right;
 public:
-    Node(const int key, std :: string data){
+    Node(int key, std :: string data){
         this->key = key;
         this->data = data;
         left = nullptr;
@@ -29,36 +29,42 @@ private:
         else if (root->key > key) return lookForNode(root->left, key);
         else return lookForNode(root->right, key);
     }
-    Node* findParent  (Node * root, int key) {
+    Node* findParentWhileAdding  (Node * root, int key) {
         if (root->key <= key) {
             if (root->right == nullptr)
                 return root;
-            return findParent(root->right, key);
+            return findParentWhileAdding(root->right, key);
         }
         if (root->key > key) {
             if (root->left == nullptr)
                 return root;
-            return findParent(root->left, key);
+            return findParentWhileAdding(root->left, key);
         }
         return nullptr;
     }
+    Node* findParentWhileDeliting(Node* root, int key){
+        if (root->right->key == key) return root;
+        if (root->left->key == key) return root;
+        if (root->key<=key) return findParentWhileDeliting(root->right,key);
+        return findParentWhileDeliting(root->left,key);
+    };
     Node* min(Node* root)
     {
-        Node* findMin = root;
-        while (findMin->left != nullptr)
-        {
-            findMin = findMin->left;
-        }
-        return findMin;
+        if (root->left == nullptr)
+            return root;
+        else
+            return min(root->left);
     }
 public:
     bool add(const int key, std:: string data) {
         Node* child = new Node (key,data);
+        if (lookForNode(root,key)!= nullptr)
+            return false;
         if (root == nullptr){
             root = child;
-            return false;
+            return true;
         }
-        Node* parent = findParent(root, key);
+        Node* parent = findParentWhileAdding(root, key);
         if (key <= parent->key)
             parent->left = child;
         else
@@ -67,7 +73,7 @@ public:
     };  // false if key already exists
     bool del(const int key){
         Node* deletedNode = lookForNode(root,key);
-        Node* parentOfDeletedNode = findParent(root, key);
+        Node* parentOfDeletedNode = findParentWhileDeliting(root, key);
         if (deletedNode == nullptr)
             return false;
         if (deletedNode->left == nullptr && deletedNode->right == nullptr)
@@ -126,16 +132,23 @@ public:
         }
     }; // false if no key
     std::string find(const int key) {
-        if (root == nullptr)
-            return " ";
-        if (root->key > key){
-            root = root->left;
-            return find(key);
+        Node* node = root;
+        if (node == nullptr)
+            return "2";
+        while (node->key != key)
+        {
+            if (key > node->key)
+            {
+                if (node->right == nullptr) return " ";
+                else node = node->right;
+            }
+            else if (key < node->key)
+            {
+                if (node->left == nullptr) return " ";
+                else node = node->left;
+            }
         }
-        else{
-            root = root->right;
-            return find (key);
-        }
+        return node->data;
     };  // return '' if no key
 public:
     Tree(){
@@ -146,5 +159,18 @@ public:
 
 
 int main() {
+    Tree testTree;
+    testTree.add(33, "3");
+    testTree.add(31,"5");
+    testTree.add(150,"/");
+    testTree.add(6,"2");
+    testTree.add(33, "<3");
+    std::cout<<testTree.find(33)<<std::endl;
+    std::cout<<testTree.find(0)<<std::endl;
+    std::cout<<testTree.find(8)<<std::endl;
+    std::cout<<testTree.find(6)<<std::endl;
+    testTree.del(150);
+    std::cout<<testTree.find(150)<<std::endl;
+    std::cout<<testTree.find(31)<<std::endl;
     std::cout << "The end." << std::endl;
 }
