59d1214a518c8d47a07de3675ec38d21d97d0431
QuoZer <m.d.panteleev+github@gmail.com>
2020-11-29 21:05:05 +0300

    Yay! List + tests

--- PanteleevMD/checkpoint00/list.cxx
+++ PanteleevMD/checkpoint00/list.cxx
@@ -112,8 +112,9 @@ void Iterator::del()
     if (current && prev)       //if the list is not empty and previous element present
     {
         prev->next = current->next;     //ties neighbours together
-        delete current;
-    }                                   //no need in updating head
+        delete current;                    //no need in updating head
+        current = prev->next;
+    }                                   
     else if (current == master->root)
     {
         master->root = current->next;   //update head
@@ -165,26 +166,44 @@ Iterator::~Iterator()
 
 int main() {
     List test;
-    Iterator it = test.begin(); 
 
+    std::cout << "Simple test" << '\n';
+    Iterator it = test.begin(); 
     it.insert(1);
     it.insert(2);
     it.insert(3);
     it.insert(4);
     it.insert(5);
-
     for (; it.get_value() != NULL; it.next())
     {
         std::cout << it.get_value()  << '\n';
     }
 
-    std::cout << it.get_value() << '\n';
-    it.del();
-    std::cout << it.get_value() << '\n';
-    it.set_value(71);
-    std::cout << it.get_value() << '\n';
-    it = it.next();
-    std::cout << it.get_value() << '\n';
+    std::cout << "Second iterator test" << '\n';
+    Iterator it2 = test.begin();
+
+    std::cout << it2.get_value() << '\n';
+    it2.del();
+    std::cout << it2.get_value() << '\n';
+    it2.set_value(71);
+    std::cout << it2.get_value() << '\n';
+    it2 = it2.next();
+    std::cout << it2.get_value() << '\n';
+
+    std::cout << "Delete middle element test" << '\n';
+    Iterator it3 = test.begin();
+    it3.set_value(4);
+    it3.insert(5);
+
+    it3.next();
+    it3.next();
+    it3.del();
+    it3.del();
+
+    for (Iterator it4 = test.begin(); it4.get_value() != NULL; it4.next())
+    {
+        std::cout << it4.get_value()  << '\n';
+    }
 
     std::cout << "The end." << std::endl;
 }
