2e0238ab2d4001f28182fd24530dc01fe199a479
Serov_danila <70762407+SerovDanila@users.noreply.github.com>
2020-12-06 23:57:23 +0300

    Убрал инициализацию из интерфейса

--- Serov/checkpoint/List/main.cpp
+++ Serov/checkpoint/List/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 class List;
 
 class Node {
@@ -6,14 +7,8 @@ public:
     int value;
     Node *next;
 public:
-    Node(int value){
-        this->value = value;
-        next = nullptr;
-    }
-    Node(int value, Node *next){
-        this-> value = value;
-        this-> next = next;
-    }
+    Node(int value);
+    Node(int value, Node *next);
 };
 
 
@@ -23,19 +18,9 @@ private:
     Node *prev;
     List *head;
 public:
-    Iterator next(){
-        if (current != nullptr){
-            prev = current;
-            current = current->next;
-        }
-        return *this;
-    };
-    int get_value(){
-        return current->value;
-    };
-    void set_value(const int value){
-        current->value = value;
-    };
+    Iterator next();
+    int get_value();
+    void set_value(const int value);
 public:
     void insert(const int value);
     void del();
@@ -53,8 +38,18 @@ public:
     Iterator begin();
 public:
     List();
+    ~List();
 };
 
+Node :: Node(int value){
+    this->value = value;
+    next = nullptr;
+}
+
+Node ::Node(int value, Node *next) {
+    this-> value = value;
+    this-> next = next;
+}
 
 Iterator :: Iterator(){
     current = head->root;
@@ -75,31 +70,53 @@ List ::List() {
     root = nullptr;
 }
 
+List :: ~List() {
+    Iterator toDelete;
+    while (root != nullptr)
+        toDelete.del();
+}
+
 void Iterator::insert (const int value){
     current = new Node(value, current);
     head->root = current;
 }
 
 void Iterator::del() {
-    if (current != nullptr && prev != nullptr)       
+    if (current != nullptr && prev != nullptr)
     {
         prev->next = current->next;
         delete current;
         current = prev->next;
-	return;
+        return;
     }
     if (prev == nullptr && current != nullptr)
     {
         head->root = current->next;
         delete current;
         current = head->root;
-	return;    
+        return;
     }
 }
 
+int Iterator  :: get_value() {
+    return current->value;
+}
+
+void Iterator ::set_value(const int value) {
+    current->value = value;
+}
+
+Iterator Iterator ::next() {
+    if (current != nullptr){
+        prev = current;
+        current = current->next;
+    }
+    return *this;
+}
+
+
 int main() {
     List list;
-
     Iterator iterator = list.begin();
     iterator.insert(1);
     iterator.insert(2);
@@ -110,4 +127,3 @@ int main() {
     std::cout << iterator.get_value() << '\n';
     std::cout << "The end." << std::endl;
 }
-
