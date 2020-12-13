882dd2f4c2b9f3e492537f0dcd844c849d09db01
Kalyasova_Svetlana <70764801+KalyasovaSvetlana@users.noreply.github.com>
2020-12-14 01:07:56 +0300

    Add files via upload

--- KalyasovaSvetlana/checkpoint/stack/main.cpp
+++ KalyasovaSvetlana/checkpoint/stack/main.cpp
@@ -1,43 +1,53 @@
 #include <iostream>
-
+const int err = 666666666;
 
 class Stack {
 private:
-    struct stek
-    {
-        int value;
-        struct stek *next;
-    };
-    struct stek *NEXT;
+    struct stack;
+    struct stack *peak;
 public:
-    int pop(){
-        int temp = NEXT->value;
-        stek *MyStack = NEXT;
-        NEXT = NEXT->next;
-        delete MyStack;
-        return temp;
-    }
-    void push(const int value){
-        stek *MyStack = new stek;
-        MyStack->value = value;
-        MyStack->next = NEXT;
-        NEXT = MyStack;
-    };
+    int pop();
+    void push(const int value);
 public:
-    Stack(){
-        NEXT = NULL;
-    };
-    ~Stack() {
-        while (NEXT) {
-            stek *element = NEXT;
-            NEXT = NEXT->next;
-            free(element);
-        }
-    }
+    Stack();
+    ~Stack();
 };
 
+struct Stack::stack{
+    int value;
+    struct stack *next;
+};
 
+Stack::~Stack() {
+    while (peak) {
+        stack *element = peak;
+        peak = peak->next;
+        delete(element);
+    }
+}
+
+int Stack::pop(){
+    if (!peak)
+        return err;
+    int temp = peak->value;
+    stack *MyStack = peak;
+    peak = peak->next;
+    delete MyStack;
+    return temp;
+}
+
+Stack::Stack() {
+    peak = nullptr;
+}
+
+void Stack::push(const int value) {
+    stack *MyStack = new stack;
+    MyStack->value = value;
+    MyStack->next = peak;
+    peak = MyStack;
+}
 
 int main() {
     std::cout << "The end." << std::endl;
 }
+
