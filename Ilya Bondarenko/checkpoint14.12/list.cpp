cf7deed343b7c5f0ed2a4b67e4c2b72025ec0362
BondarenkoIlya4 <70769835+BondarenkoIlya4@users.noreply.github.com>
2020-12-14 12:03:54 +0300

    Add files via upload

---
+++ Ilya Bondarenko/checkpoint14.12/list.cpp
@@ -0,0 +1,141 @@
+﻿#include <iostream>
+#define POP_ERROR 000;
+
+class Node {
+public:
+    int value;
+    Node* next;
+public:
+    Node(int value);
+    Node(int value, Node* next);
+};
+
+
+class Iterator {
+private:
+    Node* current;
+    Node* prev;
+    List* list;
+public:
+    Iterator next();
+    int get_value();
+    void set_value(const int value);
+public:
+    void insert(const int value);  // insert new node after current
+    void del();  // delete current node
+public:
+    Iterator();
+    Iterator(List& list);
+};
+
+class List {
+private:
+    friend class Iterator;
+    Node* root;
+public:
+    Iterator begin();
+public:
+    List();
+    ~List();
+};
+
+Node::Node(const int value)
+{
+    this->value = value;
+    this->next = nullptr;
+}
+
+Node::Node(const int value, Node* next)
+{
+    this->value = value;
+    this->next = next;
+}
+
+List::List() {
+    root = nullptr;
+}
+
+List::~List()
+{
+    Iterator NodeIter;
+    while (root != nullptr)
+        NodeIter.del();
+}
+
+Iterator::Iterator(List& list)
+{
+    current = list.root;
+    prev = nullptr;
+    this->list = &list;
+}
+
+Iterator Iterator::next()
+{
+    if (current->next == nullptr)
+    {
+        prev = nullptr;
+        current = nullptr;
+        return *this;
+    }
+    prev = current;
+    current = current->next;
+    return *this;
+}
+
+int Iterator::get_value()
+{
+    if (current != nullptr)
+        return current->value;
+    return POP_ERROR;
+}
+
+void Iterator::set_value(const int value)
+{
+    if (current != nullptr)
+        current->value = value;
+}
+
+void Iterator::del()
+{
+    if (current == list->root) {
+        list->root = list->root->next;
+        delete current;
+        current = list->root;
+        return;
+    }
+
+    if (current->next == nullptr)
+    {
+        prev->next = nullptr;
+        delete current;
+        current = list->root;
+        return;
+    }
+
+    prev->next = current->next;
+    delete current;
+    current = list->root;
+}
+
+void Iterator::insert(const int value)
+{   
+    if (prev == nullptr)
+    {
+        list->root = new Node(value, current);
+    }
+    else 
+    {
+        prev->next = new Node(value, current);
+    }
+}
+
+Iterator List::begin()
+{
+    Iterator tmp(*this);
+    return (tmp);
+}
+
+
+int main() {
+    std::cout << "The end." << std::endl;
+}
---
+++ Ilya Bondarenko/checkpoint14.12/stack.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <stack>
+#define POP_ERROR 000;
+
+class Stack {
+
+public:
+    int pop();
+    void push(const int value);
+public:
+    Stack();
+    ~Stack();
+}
+
+struct Element
+{
+    int value;
+    struct Element* next;
+};
+
+struct Element* Top;
+
+void Stack::push(const int value) {
+    struct Element* point;
+    point = new Element;
+    point-> value = value;
+    point->next = Top;
+    Top = point;
+}
+
+int Stack::pop() {
+    if (Top == nullptr) {
+        return POP_ERROR;
+    }
+    int temp = Top-> value;
+    Element* pointer = Top;
+    Top = Top->next;
+    delete pointer;
+    pointer = nullptr;
+    return temp;
+}
+
+Stack::Stack() {
+    Top = nullptr;
+}
+
+Stack::~Stack() {
+    while (Top != nullptr) {
+        Element* pp = Top;
+        Top = Top->next;
+        delete pp;
+    }
+}
+
+int main() {
+    std::cout << "The end." << std::endl;
+}
