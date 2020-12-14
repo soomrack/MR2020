d0e814a6c48edf8cfe411a5267e6b6fcb5bd517a
rakovAndrew <70765297+rakovAndrew@users.noreply.github.com>
2020-12-14 10:42:21 +0300

    Create stack.cpp

---
+++ Rakov/checkpoint/stack/stack.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+struct component{
+
+    int value; // a value of the component
+    component *nextComponent; // a pointer to the next component
+
+};
+
+
+class Stack{
+ 
+    public:
+        int pop();
+        void push(const int value);
+
+        Stack();
+        ~Stack();
+
+    private:
+
+        component *top;
+
+};
+
+Stack::Stack() :top(nullptr){}
+
+Stack::~Stack(){
+
+    while (top){
+
+        component *currentComponent = top;
+        top = top->nextComponent;
+        delete currentComponent;
+
+    } 
+
+}
+
+int Stack::pop(){
+
+    if (top == nullptr){
+
+        return -1;
+
+    }
+
+    int value = top->value;
+    component *currentComponent = top;
+    top = top->nextComponent;
+    delete currentComponent;
+
+    return value;
+
+}
+
+void Stack::push(const int value){
+
+    component *newComponent = new component;
+    newComponent->value = value;
+    newComponent->nextComponent = top;
+    top = newComponent;
+
+}
+
+int main() {
+
+    Stack myStack;
+
+    myStack.push(5);
+    myStack.push(4);
+    myStack.push(3);
+    myStack.pop();
+    myStack.pop();
+    int a = myStack.pop();
+    cout << a << endl;
+    
+}
