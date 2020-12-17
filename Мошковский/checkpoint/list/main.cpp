89305c3092df265fc7d8f5480f392dce15b0f14a
georgii-mo <70721371+georgii-mo@users.noreply.github.com>
2020-12-17 23:36:28 +0300

    Add files via upload

--- Мошковский/checkpoint/list/main.cpp
+++ Мошковский/checkpoint/list/main.cpp
@@ -1,195 +1,217 @@
 #include <iostream>
-#include <string>
 
+class List;
+class Iterator;
 
-class Node{
+class Node {
 public:
     int value;
     Node *next;
 public:
-    Node(int value = 0, Node *next = nullptr);
-
+    Node();
+    Node( int value);
+    Node( int value,  Node *next);
+    ~Node();
 };
-
-Node::Node(int value, Node* next)
-{
-    this->value = value;
-    this->next = next;
-}
-
-
-class List;
 
 class Iterator {
 private:
+    List *master;
     Node *current;
     Node *prev;
-    List *list;
 public:
     Iterator next();
     int get_value();
     void set_value(const int value);
 public:
-    void insert(const int value);
-    void del();
+    void insert(const int value);  // insert new node after current
+    void del();  // delete current node
 public:
-    Iterator(List & lst);
+    Iterator();
+    Iterator(List &master);
+    ~Iterator();
 };
 
 class List {
 private:
-
     friend class Iterator;
-    Node *root;
-    int size;
-
+    Node *root;     //more like head but ok
 public:
     Iterator begin();
 public:
     List();
     ~List();
-    void popFront();
-    void popBack();
-    void clear();
-    void pushBack(int value);
-    int getSize() const;
 };
 
 
-List::List(){
-    size = 0;
+Node::Node()
+{
+    next = nullptr;
+}
+
+Node::Node(int value)
+{
+    this->value = value;
+}
+
+Node::Node( int value,  Node *next)
+{
+    this->value = value;
+    this->next = next;
+}
+
+Node::~Node()
+{
+    //delete this;
+}
+
+List::List()
+{
     root = nullptr;
+}
+
+Iterator List::begin()
+{
+    Iterator fresh(*this);
+    return fresh;
 }
 
 List::~List()
 {
-    clear();
-}
-
-void List::clear()
-{
-    while (root!=nullptr) popFront();
-}
-
-void List::popFront()
-{
-    if (root == nullptr) return;
-    Node* temp = root;
-    root = root->next;
-    delete temp;
-    size--;
-}
-
-void List::pushBack(int value)
-{
-    if (root == nullptr)
-        root = new Node(value);
-    else
+    Iterator eraser;
+    while(root)     //deletes elements untill nullptr is reached
     {
-        Node* current = root;
-        while (current->next == nullptr)
-        {
-            current->next = new Node(value);
-            break;
-        }
+        eraser.del();
     }
-    size++;
 }
 
 
-int List::getSize() const
+Iterator::Iterator()
 {
-    return size;
-}
-
-
-Iterator List::begin(){
-    Iterator tmp(*this);
-
-    return tmp;
-}
-
-
-Iterator::Iterator(List & lst){
-    current = lst.root;
+    current = master->root;
     prev = nullptr;
-    list = &lst;
 }
 
+Iterator::Iterator(List &masterList)
+{
+    master = &masterList;
+    current = master->root;
+    prev = nullptr;
+}
 
-Iterator Iterator::next(){
-    if (this->current->next == nullptr){
-        this->prev = this->current;
-        this->current = this->list->root;
-        return *this;
+void Iterator::insert(const int value)
+{
+    Node* tempNode = new Node(value, current);
+    if (prev != nullptr)
+    {
+        prev->next = tempNode;
+        return;
     }
-    else{
-        this->prev = this->current;
-        this->current = this->current->next;
-        return *this;
+    current = tempNode;
+    master->root = current;
+}
+
+void Iterator::del()
+{
+    if (current && prev)       //if the list is not empty and previous element present
+    {
+        prev->next = current->next;     //ties neighbours together
+        delete current;                    //no need in updating head
+        current = prev->next;
+    }
+    else if (current == master->root)
+    {
+        master->root = current->next;   //update head
+        delete current;
+        current = master->root;
     }
 
+    //next();
 }
 
-int Iterator::get_value(){
-    return current->value;
+int Iterator::get_value()
+{
+    if (current)        //not null
+    {
+        return current->value;
+    }
+    return NULL;
 }
 
-void Iterator::set_value(const int value){
-    if (current != nullptr){
+void Iterator::set_value(int value)
+{
+    if (current)        //not null
+    {
         current->value = value;
     }
-
 }
 
-void Iterator::insert(const int value){
-    Node* previous = current;
-    previous->next = new Node(value, previous->next);
-    list->size++;
+Iterator Iterator::next()
+{
+    if (this->current == nullptr) return *this;       //idk what to retun (no .end())
+
+    this->prev = this->current;
+    this->current = this->current->next;
+
+    return *this;
 }
 
-void Iterator::del(){
-    Node* temp = this->current;
-    if(temp->next == nullptr){
-        prev->next = temp->next;
-        current = list->root;
-    }
-    else{
-
-        prev->next = temp->next;
-        current = temp->next;
-    }
-
-    delete temp;
-    list->size--;
+Iterator::~Iterator()
+{
+    //delete this;
 }
 
 
 int main() {
-    Node nd;
+    List test;
+    std::cout << "Simple test" << '\n';
+    Iterator it = test.begin();
+    it.insert(1);
+    it.insert(2);
+    it.insert(3);
+    it.insert(4);
+    it.insert(5);
+    for (; it.get_value() != NULL; it.next())
+    {
+        std::cout << it.get_value()  << '\n';
+    }
 
-    List list;
-    list.pushBack(1);
-    list.pushBack(2);
-    list.pushBack(3);
-    list.pushBack(4);
+    std::cout << "Second iterator test" << '\n';
+    Iterator it2 = test.begin();
 
-    Iterator it = list.begin();
-    it.next();
-    it.next();
-    it.next();
-    it.next();
-    it.next();
-    int a = it.get_value();
-    it.set_value(100);
-    int b = it.get_value();
+    std::cout << it2.get_value() << '\n';
+    it2.del();
+    std::cout << it2.get_value() << '\n';
+    it2.set_value(71);
+    std::cout << it2.get_value() << '\n';
+    it2 = it2.next();
+    std::cout << it2.get_value() << '\n';
 
-    it.next();
-    it.insert(10);
-    it.del();
-    it.del();
+    std::cout << "Delete middle element test" << '\n';
+    Iterator it3 = test.begin();
+    it3.set_value(4);
+    it3.insert(5);
+
+    it3.next();
+    it3.next();
+    it3.del();
+    it3.del();
+
+    for (Iterator it3 = test.begin(); it3.get_value() != NULL; it3.next())
+    {
+        std::cout << it3.get_value()  << '\n';
+    }
+
+    std::cout << "Middle insertion test" << '\n';
+    Iterator it4 = test.begin();
+    it4.next();
+    it4.next();
+    it4.insert(99);
+    for (Iterator it4 = test.begin(); it4.get_value() != NULL; it4.next())
+    {
+        std::cout << it4.get_value()  << '\n';
+    }
 
 
-
-    return 0;
+    std::cout << "The end." << std::endl;
 }
