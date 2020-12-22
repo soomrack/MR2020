631c08786fe8019b87dd6a763d1d47a653925645
leontievln <70767306+leontievln@users.noreply.github.com>
2020-12-23 00:43:24 +0300

    Add files via upload

---
+++ leontiev/checkpoint00/list.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+#include <string>
+
+const int ERROR_NULLPTR = -1;
+
+class Node {
+public:
+    int value;
+    Node* next;
+public:
+    Node(int value = 0, Node* next = nullptr);
+};
+
+Node::Node(int value, Node* next)
+{
+    this->value = value;
+    this->next = next;
+}
+
+class List;
+
+class Iterator {
+private:
+    Node* current;
+    Node* prev;
+    List* list;
+public:
+    Iterator next();
+    int get_value();
+    void set_value(const int value);
+public:
+    void insert(const int value); 
+    void del();  
+public:
+    Iterator(List& lst);
+};
+
+
+class List {
+private:
+    friend class Iterator;
+    Node* root;
+    int size;
+public:
+    Iterator begin();
+public:
+    List();
+    ~List();
+    void popFront();
+    void clear();
+    void pushBack(int value);
+    int getSize() const;
+};
+
+
+List::List() {
+    size = 0;
+    root = nullptr;
+}
+
+List::~List()
+{
+    clear();
+}
+
+void List::clear()
+{
+    while (root != nullptr) popFront();
+}
+
+void List::popFront()
+{
+    if (root == nullptr) return;
+    Node* temp = root;
+    root = root->next;
+    delete temp;
+    size--;
+}
+
+void List::pushBack(int value)
+{
+    if (root == nullptr)
+        root = new Node(value);
+    else
+    {
+        Node* current = root;
+        while (current->next != nullptr) current = current->next;
+        current->next = new Node(value);
+    }
+    size++;
+}
+
+int List::getSize() const
+{
+    return size;
+}
+
+Iterator List::begin() {
+    Iterator tmp(*this);
+    return tmp;
+}
+
+Iterator::Iterator(List& list) {
+    current = list.root;
+    prev = nullptr;
+    this->list = &list;
+}
+
+Iterator Iterator::next() {
+    if (current == nullptr) {
+        return *this;
+    }
+    if (current->next == nullptr) {
+        prev = current;
+        current = list->root;
+        return *this;
+    }
+    prev = current;
+    current = current->next;
+    return *this;
+}
+
+int Iterator::get_value() {
+    if (current == nullptr) {
+        return ERROR_NULLPTR;
+    }
+    return current->value;
+}
+
+void Iterator::set_value(const int value) {
+    if (current != nullptr) {
+        current->value = value;
+    }
+}
+
+void Iterator::insert(const int value) {
+    if (current == nullptr) {
+        return;
+    }
+    current->next = new Node(value, current->next);
+    list->size++;
+}
+
+void Iterator::del() {
+    if ((current == nullptr) || (prev == nullptr)) {
+        return;
+    }
+    Node* temp = this->current;
+    if (temp->next == nullptr) {
+        prev->next = temp->next;
+        current = list->root;
+    }
+    else {
+
+        prev->next = temp->next;
+        current = temp->next;
+    }
+    delete temp;
+    list->size--;
+}
+
+
+int main() {
+    List list;
+    list.pushBack(1);
+    list.pushBack(2);
+    list.pushBack(3);
+
+    Iterator it = list.begin();
+    it.next();
+    it.next();
+    it.next();
+    int a = it.get_value();
+    it.set_value(100);
+    int b = it.get_value();
+
+    it.next();
+    it.insert(10);
+    it.del();
+    it.del();
+
+    std::cout << "The end." << std::endl;
+
+    return 0;
+}
---
+++ leontiev/checkpoint00/stack.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+class Stack {
+public:
+    int pop();
+    void push(const int value);
+public:
+    Stack();
+    ~Stack();
+private:
+    int top;
+    int* data;
+};
+
+
+int main() {
+    Stack stk;
+
+    stk.push(11);
+    stk.push(111);
+    stk.push(1111);
+    stk.push(33333);
+
+    std::cout << stk.pop() << ' ' << stk.pop() << ' ' << stk.pop() << ' ' << stk.pop();
+    std::cout << "\nThe end." << std::endl;
+    return 0;
+}
+
+
+Stack::Stack() {
+    this->top = 0;
+    this->data = new int[1];
+    this->data[0] = 0;
+}
+
+Stack::~Stack() {
+    this->top = 0;
+    delete[] this->data;
+}
+
+void Stack::push(const int value) {
+    this->top++;
+    int* copy = new int[this->top];
+    for (int i = 0; i < this->top - 1; i++) {
+        copy[i] = this->data[i];
+    }
+    delete[] this->data;
+    this->data = copy;
+    this->data[this->top - 1] = value;
+
+}
+
+int Stack::pop() {
+    this->top--;
+    int elem = this->data[this->top];
+    int* copy = new int[this->top];
+    for (int i = 0; i < this->top; i++) {
+        copy[i] = this->data[i];
+    }
+    delete[] this->data;
+    this->data = copy;
+    return elem;
+
+}
