fb25672918d35b8cc87e40444f2db30dd8762b75
MOShka78 <64366438+MOShka78@users.noreply.github.com>
2020-12-22 13:18:39 +0300

    Add files via upload

--- Елисеев Вадим/checkpoint/list.cpp
+++ Елисеев Вадим/checkpoint/list.cpp
@@ -1,102 +1,133 @@
-﻿
-#include <iostream>
-using namespace std;
+﻿#include <iostream>
 
-typedef int newtp;
-struct node
-{
-    newtp data; //  данные 
-    node* next; // указатель на следующий
-    node* pred; // указатель на предыдущий
-};
+class List;
+class Iterator;
 
-class linklist // класс связный список
-{
-private:
-    node* first; // указатель на начало
+class Node {
 public:
-    linklist() { first = NULL; } // в конструкторе инициализируем его, он указывает в 
-                                 // в место где нет полезной информации
-
-    void push(newtp d, int pos); // метод добавления элемента
-    int pop(int pos);  // метод удаления элемента
-    void clean(); // удаление всех элементов
-    void view(); // вывод на экран
+    int value;
+    Node* next;
+public:
+    Node();
+    Node(int value);
+    Node(int value, Node* next);
 };
 
-void linklist::push(newtp d, int pos) // метод добавления элемента
-{
+class Iterator {
+private:
+    Node* current;
+    Node* prev;
+    List* list;
+public:
+    Iterator next();
+    int get_value();
+    void set_value(int value);
+public:
+    void insert(int value);  
+    void del();  
+public:
+    Iterator(List& list);
+};
 
-    node* newnode = new node; // создаём новый элемент
-    newnode->data = d; // вводим в него данные
-    if (first == NULL)   // если это первый элемент в списке                                   
-    {
-        newnode->next = newnode;
-        newnode->pred = newnode;
-        first = newnode; // first  указывает на него
+
+class List {
+private:
+    friend class Iterator;
+    Node* root;
+public:
+    Iterator begin();
+public:
+    List();
+};
+
+Node::Node() {
+    next = nullptr;
+}
+
+Node::Node(int value) {
+    this->value = value;
+}
+
+Node::Node(int value, Node* next) {
+    this->value = value;
+    this->next = next;
+}
+
+
+List::List() {
+    root = nullptr;
+}
+
+Iterator List::begin() {
+    Iterator newNode(*this);
+    return newNode;
+}
+
+
+Iterator::Iterator(List& list) {
+    current = list.root;
+    prev = nullptr;
+    this->list = &list;
+}
+
+void Iterator::insert(int value) {
+    Node* tempNode = new Node(value, current);
+    if (prev != nullptr) {
+        prev->next = tempNode;
+        return;
     }
-    else
-    {
-        node* temp = first; // создаём времменный указатель
-        for (int i = pos; i > 1; i--, temp = temp->next); // цикл
-        temp->pred->next = newnode;
-        newnode->pred = temp->pred;
-        newnode->next = temp; // добавляем перед времменным
-        temp->pred = newnode;
+    current = tempNode;
+    list->root = current;
+}
+
+void Iterator::del() {
+    if (current == list->root) {
+        list->root = current->next;
+        delete current;
+        current = list->root;
+        return;
+    }
+    if (current && prev) {
+        prev->next = current->next;
+        delete current;
+        current = prev->next;
     }
 }
 
-int linklist::pop(int pos) // удаляем элемент из списка по индексу
-{
-    if (first == NULL)  return 0; // если список пуст
-    int val;
-    if (first == first->next) // если это последний элемент в списке
-    {
-        val = first->data;
-        delete first;
-        first = NULL;
+int Iterator::get_value() {
+    if (current != nullptr) {
+        return current->value;
     }
-    else
-    {
-        node* temp = first;
-        for (int i = pos; i > 1; i--, temp = temp->next);
-        if (temp == first) first = temp->next;
-        temp->pred->next = temp->next; // удаляем temp элемент
-        temp->next->pred = temp->pred;
-        val = temp->data;
-        delete temp;
+    return NULL;
+}
+
+void Iterator::set_value(int value) {
+    if (current != nullptr) {
+        current->value = value;
     }
-    return val;
 }
 
-void linklist::clean() // удалить все элементы из списка
-{
-    if (first == NULL) return;
-    for (node* newnode = first->next; newnode != first; newnode = newnode->next) delete newnode;
-    delete first;
-    first = NULL;
+Iterator Iterator::next() {
+    if (this->current == nullptr) {
+        this->prev = this->current;
+        this->current = nullptr;
+        return *this;
+    }
+    prev = current;
+    current = current->next;
+    return *this;
 }
 
-void linklist::view() // просмотр элементов в списке
-{
-    if (first == NULL) return;
-    node* newnode = first;
-    do
-    {
-        cout << newnode->data << endl;
-        newnode = newnode->next;
-    } while (newnode != first);
-}
+int main() {
+    List test;
 
-int main()
-{
-    linklist li;
+    Iterator iterator = test.begin();
+    iterator.insert(1);
+    iterator.insert(2);
+    iterator.insert(3);
 
-    li.push(12, 1);
-    li.push(13, 2);
-    li.push(14, 3);
-
-    li.view();
-
-
+    for (; iterator.get_value() != NULL; iterator.next()) {
+        std::cout << iterator.get_value() << '\n';
+    }
+    std::cout << "The end." << std::endl;
 }
