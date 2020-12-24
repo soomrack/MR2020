256b751d624b729d8f16f3ad65add81cbd1837b2
rakovAndrew <70765297+rakovAndrew@users.noreply.github.com>
2020-12-24 22:39:15 +0300

    I deleted unuseful spaces and gapes

--- Rakov/checkpoint/stack/stack.cpp
+++ Rakov/checkpoint/stack/stack.cpp
@@ -1,71 +1,53 @@
 #include <iostream>
 using namespace std;
 
-struct component{
+#define ERROR_OCCURED -1488
 
+struct component{
     int value; // a value of the component
     component *nextComponent; // a pointer to the next component
-
 };
 
-
 class Stack{
- 
     public:
         int pop();
         void push(const int value);
-
         Stack();
         ~Stack();
-
     private:
-
         component *top;
-
 };
 
 Stack::Stack() :top(nullptr){}
 
 Stack::~Stack(){
-
     while (top){
-
         component *currentComponent = top;
         top = top->nextComponent;
         delete currentComponent;
-
     } 
-
 }
 
 int Stack::pop(){
-
     if (top == nullptr){
-
-        return -1;
-
+        return ERROR_OCCURED;
     }
-
     int value = top->value;
     component *currentComponent = top;
     top = top->nextComponent;
     delete currentComponent;
 
     return value;
-
 }
 
 void Stack::push(const int value){
-
     component *newComponent = new component;
     newComponent->value = value;
     newComponent->nextComponent = top;
     top = newComponent;
-
 }
 
 int main() {
-
     Stack myStack;
 
     myStack.push(5);
@@ -75,5 +57,4 @@ int main() {
     myStack.pop();
     int a = myStack.pop();
     cout << a << endl;
-    
 }
