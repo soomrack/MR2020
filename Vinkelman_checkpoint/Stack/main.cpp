a3ac7cd3af1c7a4a715464772c31e37ea53d436b
AlexanderVinkelman <70704315+AlexanderVinkelman@users.noreply.github.com>
2020-12-24 22:00:38 +0300

    Add files via upload

--- Vinkelman_checkpoint/Stack/main.cpp
+++ Vinkelman_checkpoint/Stack/main.cpp
@@ -17,36 +17,40 @@ private:
     Znach *top;
 };
 
+// Создание стека
 Stack::Stack() {
     top = nullptr;
 }
 
+// Добавление нового элемента в стек
 void Stack::push(const int value) {
-    Znach *pp = nullptr;
-    pp = new Znach;
-    pp -> v = value;
-    pp -> p = top;
-    top = pp;
+    Znach *pp = nullptr;    // Объявляется новый указатель
+    pp = new Znach;         // Выделение памяти под новый указатель
+    pp -> v = value;        // Переменной value присваивается значение переданной переменной
+    pp -> p = top;          // Ссылаемся на предыдущий элемент (top)
+    top = pp;               // Изменение ссылки на последний элемент
 }
 
+// Изъятие верхнего элемента из стека
 int Stack::pop() {
-    if (top == nullptr){
+    if (top == nullptr){    // Проверяем стек на наличие пустых значений
         return -1;
     }
-    int value = top -> v;
-    Znach *pp = top;
-    top = top -> p;
-    delete pp;
-    pp = nullptr;
-    return value;
+    int value = top -> v;    // Сохранение значения верхнего элемента
+    Znach *pp = top;         // Создание указателя на последний элемент
+    top = top -> p;          // Меняем ссылку на последней элемент на ссылку на предпоследний
+    delete pp;               // Высвобождение памяти
+    pp = nullptr;            // Замена указателя на нуль-указатель
+    return value;            // Возвращение значения верхнего элемента
 }
 
+// Высвобождение стека
 Stack::~Stack() {
     while (top != nullptr){
-        Znach *pp = top;
-        top = top -> p;
-        delete pp;
-        pp = nullptr;
+        Znach *pp = top;    // Создание указателя на последний элемент
+        top = top -> p;     // Меняем ссылку на последний элемент на ссылку на предпоследний
+        delete pp;          // Высвобождение памяти
+        pp = nullptr;       // Замена указателя на нуль-указатель
     }
 }
 
