abd25734fd557e1867357502cef8a4a984d0a49a
potekhin2020 <70805817+potekhin2020@users.noreply.github.com>
2020-12-25 05:54:06 +0300

    Add files via upload

---
+++ Aleksander Potekhin/checkpoint 25.12/Stack.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+const int POP_ERROR = -1;
+
+class Stack {
+private:
+    struct Node;
+    Node* top;
+
+public:
+    int pop();
+    void push(const int value);
+public:
+    Stack();
+    ~Stack();
+};
+
+struct Stack::Node {
+    int content;
+    Node* point;
+};
+
+int Stack::pop() {
+    if (!top) {
+        return POP_ERROR;
+    }
+    int end = top->content;
+    Node* targetNode = top;
+    top = top->point;
+    delete targetNode;
+    return end;
+}
+
+void Stack::push(const int value) {
+    Node* temp;
+    temp = new Node;
+    temp->content = value;
+    temp->point = top;
+    top = temp;
+};
+
+Stack::Stack() :top(NULL) {}
+
+Stack::~Stack() {
+    while (top) {
+        Node* half = top;
+        top = top->point;
+        delete (half);
+    }
+}
+int main() {
+    std::cout << "The end" << std::endl;
+}
