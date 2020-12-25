e9be33f0b012e3f4f42e67a04768d7158e8cefed
khusnutdinova-elina <70758189+khusnutdinova-elina@users.noreply.github.com>
2020-12-25 09:17:50 +0300

    Add files via upload

--- khusnutdinova/checkpoint/list/main.cpp
+++ khusnutdinova/checkpoint/list/main.cpp
@@ -156,56 +156,7 @@ Iterator Iterator::next()
 
     return *this;
 }
-int main() {
-    List test;
-
-    std::cout << "Simple test" << '\n';
-    Iterator it = test.begin();
-    it.insert(1);
-    it.insert(2);
-    it.insert(3);
-    it.insert(4);
-    it.insert(5);
-    for (; it.get_value() != END_OF_LIST; it.next())
-    {
-        std::cout << it.get_value()  << '\n';
-    }
-
-    std::cout << "Second iterator test" << '\n';
-    Iterator it2 = test.begin();
-
-    std::cout << it2.get_value() << '\n';
-    it2.del();
-    std::cout << it2.get_value() << '\n';
-    it2.set_value(71);
-    std::cout << it2.get_value() << '\n';
-    it2 = it2.next();
-    std::cout << it2.get_value() << '\n';
-
-    std::cout << "Delete middle element test" << '\n';
-    Iterator it3 = test.begin();
-    it3.set_value(4);
-    it3.insert(5);
-
-    it3.next();
-    it3.next();
-    it3.del();
-    it3.del();
-
-    for (Iterator it3 = test.begin(); it3.get_value() != END_OF_LIST; it3.next())
-    {
-        std::cout << it3.get_value()  << '\n';
-    }
-
-    std::cout << "Middle insertion test" << '\n';
-    Iterator it4 = test.begin();
-    it4.next();
-    it4.next();
-    it4.insert(99);
-    for (Iterator it4 = test.begin(); it4.get_value() != END_OF_LIST; it4.next())
-    {
-        std::cout << it4.get_value()  << '\n';
-    }
-    
+int main()
+{
     std::cout << "The end." << std::endl;
 }
