7a5bc93731a41229e83311ca2c09818db388f018
BondarenkoIlya4 <70769835+BondarenkoIlya4@users.noreply.github.com>
2021-02-08 00:33:45 +0300

    Add files via upload

---
+++ Ilya Bondarenko/Sortirovka/Bubble_Sort.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int* array, int length) {
+    for (int sorted_index = length - 1; sorted_index > 0; sorted_index--) {
+        for (int pointer = 0; pointer < sorted_index; pointer++) {
+            if (array[pointer] > array[pointer+1])
+                swap(array[pointer], array[pointer+1]);
+        }
+    }
+}
+
+int main() {
+    int a[10] = { 5, 7, 10, 44, 52, 76, 3, 2, 1 };
+    bubbleSort(a, 10);
+}
