31bbd9679772ae259ee3cad330cc95dcf98d45a5
BondarenkoIlya4 <70769835+BondarenkoIlya4@users.noreply.github.com>
2021-02-08 11:02:07 +0300

    Add files via upload

---
+++ Ilya Bondarenko/Sortirovka/Insert_Sort.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void insertionSort(int* array, int size) {
+    for (int sorted_Index = 0; sorted_Index != size - 1; sorted_Index++) {
+        int counter = sorted_Index;
+        while ((array[counter - 1] > array[counter]) && (counter > 1)) {
+        swap(array[counter-1], array[counter ]);
+        counter--;
+            }
+    }
+}
+
+int main() {
+    int a[10] = { 5, 1, 10, 4, 3, 16, 33, 21, 11, 19 };
+    insertionSort(a, 10);
+}
