26b93287330cb4420076b331b535208adf3540fe
QuoZer <m.d.panteleev+github@gmail.com>
2021-03-22 13:06:59 +0300

    b-tree better data representation

--- PanteleevMD/B-Tree/btree.cpp
+++ PanteleevMD/B-Tree/btree.cpp
@@ -3,20 +3,21 @@
 
 #define DEBUG True
 
+#define STR_ERR ""
 #define t 3         // minimum degee
 
 class Node {
 public:
-    int num_of_keys;            // currently stored
-    int keys[2*t-1];           // keys stored in nondecreasing order
     bool is_leaf;
+    int num_of_keys;            // currently stored
+    int keys[2*t-1];            // keys stored in nondecreasing order
+    std::string data[2*t-1];
     Node* children[2*t];
-    std::string data;
 
 public:
     Node();
     Node(int keys, bool is_leaf);
-    Node(int key,  std::string data,  Node *left,  Node *right);
+    Node(int keys, bool is_leaf, std::string data);
     ~Node();
 };
 
@@ -29,18 +30,20 @@ private:
     Node* find_node(Node* search_start, const int key);
     Node* find_node(const int key);
     void split_child(Node* target, int child_gen);
-    void insert_nonfull(Node* target, const int key);
+    void insert_nonfull(Node* target, const int key, std::string data);
+    std::string tree_search(Node* target, const int key);
+// fields for Debug Visualizer VSCode addon
 #ifdef DEBUG
     std::string json_viz;
     std::string nodes;
     std::string edges;
     void visualize(Node* node, int par_id);
 #endif
+
 public:
-    void add(const int key);  
-    
+    void add(const int key, std::string data);  
     void del(const int key);  
-    std::string find(const int key);  
+    std::string find_key(const int key);  
 public:
     BTree();
     ~BTree();
@@ -59,7 +62,14 @@ Node::Node(int keys, bool is_leaf) {
     for (int b = 0; b<2*t; b++)
         this->children[b] = nullptr;
 }
-
+/*
+Node::Node(int keys, bool is_leaf) {
+    this->num_of_keys = keys;
+    this->is_leaf = is_leaf;
+    for (int b = 0; b<2*t; b++)
+        this->children[b] = nullptr;
+}
+*/
 Node::~Node() {
     // for delete children
 }
@@ -72,12 +82,32 @@ BTree::~BTree() {
     delete root;
 }
 
+std::string BTree::tree_search(Node* target, const int key)
+{
+    int index = 1;
+    Node* root = this->root;
+    while (index <= root->num_of_keys && key > root->keys[index])
+        index++;
+    if (index <= root->num_of_keys && key == root->keys[index])
+        return target->data[index];                            
+    else if (target->is_leaf)
+        return STR_ERR;
+    return tree_search(target->children[index], key);
+}
+
+std::string BTree::find_key(const int key)
+{
+    return tree_search(this->root, key);
+}
+
 void BTree::split_child(Node* target, int child_i)               // target is the node being split
 {   
     Node* child = target->children[child_i];                    // target's child_i'th child
     Node* new_node = new Node(t-1, child->is_leaf);
-    for (int j = 0; j < t-1; j++)                       
+    for (int j = 0; j < t-1; j++) {                      
         new_node->keys[j] = child->keys[j+t];
+        new_node->data[j] = child->data[j+t];
+    }
     if (child->is_leaf == false)
         for (int c = 0; c < t; c++)                     
             new_node->children[c] = child->children[c+t];       // new_node takes t largest children (w t-1 keys) from y
@@ -85,10 +115,13 @@ void BTree::split_child(Node* target, int child_i)               // target is th
     for (int j = target->num_of_keys; j > child_i + 1; j--)
         target->children[j+1] = target->children[j];
     target->children[child_i+1] = new_node;                     // new_node becomes a new child just after 'child'
-                        // ?
-    for (int j = target->num_of_keys; j > child_i; j--)         // move the median key up to separate new_node and child
+                        
+    for (int j = target->num_of_keys; j > child_i; j--) {       // move the median key up to separate new_node and child
         target->keys[j+1] = target->keys[j];
+        target->data[j+1] = target->data[j];
+    }
     target->keys[child_i] = child->keys[t-1];
+    target->data[child_i] = child->data[t-1];
     target->num_of_keys++;     
 
 #if DEBUG == True
@@ -97,16 +130,18 @@ void BTree::split_child(Node* target, int child_i)               // target is th
 }
 
 // inserts key into node x (assumed nonfull)
-void BTree::insert_nonfull(Node* target, const int key)
+void BTree::insert_nonfull(Node* target, const int key, std::string data)
 {
     int index = target->num_of_keys-1;      //
     if (target->is_leaf) {                                      // target is a leaf 
         while (index >= 0 && key < target->keys[index])
         {
             target->keys[index+1] = target->keys[index];
+            target->data[index+1] = target->data[index];
             index--;
         }
         target->keys[index+1] = key;                            // so we insert key into it
+        target->data[index+1] = data;
         target->num_of_keys++;
     }
     else {                                                      // target is not a leaf
@@ -118,7 +153,7 @@ void BTree::insert_nonfull(Node* target, const int key)
             if (key > target->keys[index])                      // decide where to descend next after the split
                 index++;
         }
-        insert_nonfull(target->children[index], key);           // no => recursevely go for the right node
+        insert_nonfull(target->children[index], key, data);           // no => recursevely go for the right node
     }
 
 #if DEBUG == True
@@ -126,7 +161,7 @@ void BTree::insert_nonfull(Node* target, const int key)
 #endif 
 }
 
-void BTree::add(const int key)
+void BTree::add(const int key, std::string data)
 {
     Node* cur_root = this->root;
     if (cur_root->num_of_keys == 2*t-1)                          // node is full
@@ -134,9 +169,9 @@ void BTree::add(const int key)
         this->root = new Node(0, false);
         this->root->children[0] = cur_root;
         split_child(this->root, 0);                             // => need split to increase height
-        insert_nonfull(this->root, key);                        // now can insert 
+        insert_nonfull(this->root, key, data);                        // now can insert 
     }
-    else insert_nonfull(cur_root, key);       
+    else insert_nonfull(cur_root, key, data);       
 }
 
 
@@ -144,27 +179,30 @@ int main()
 {
     BTree test_tree;
 
-    test_tree.add(15);
-    test_tree.add(19);
-    test_tree.add(21);
-    test_tree.add(20);
-    test_tree.add(22); 
-    test_tree.add(25); 
-    test_tree.add(30); 
-    test_tree.add(27); 
-    test_tree.add(37); 
-    test_tree.add(2); 
-    test_tree.add(34);
-    test_tree.add(5); 
-    test_tree.add(1); 
-    test_tree.add(9); 
-    test_tree.add(77);
+    test_tree.add(15, "data15");
+    test_tree.add(19, "data19");
+    test_tree.add(21, "data21");
+    test_tree.add(20, "data20");
+    test_tree.add(22, "data22"); 
+    test_tree.add(25, "data25"); 
+    test_tree.add(30, "data30"); 
+    test_tree.add(27, "data27"); 
+    test_tree.add(37, "data37"); 
+    test_tree.add(2, "data2"); 
+    std::string outpt = test_tree.find_key(25);
+    test_tree.add(34, "data34");
+    test_tree.add(5, "data5"); 
+    test_tree.add(1, "data1"); 
+    test_tree.add(9, "data9"); 
+    test_tree.add(77, "data77");
     //test_tree.del(25);
     //cout<<'wow';
     return 0;
 }
 
 
+
+// generates JSON string out of the tree to generate graphviz visualization 
 #if DEBUG == True
     void BTree::visualize(Node* node, int par_id)
     {   
