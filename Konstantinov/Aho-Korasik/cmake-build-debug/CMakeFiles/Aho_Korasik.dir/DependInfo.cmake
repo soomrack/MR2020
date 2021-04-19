76970b2ddcc50cb3bde73856806f016e91e8fb50
Alexey_Konstantinov <60509435+Poezdok@users.noreply.github.com>
2021-04-19 12:05:03 +0300

    Add files via upload

---
+++ Konstantinov/Aho-Korasik/cmake-build-debug/CMakeFiles/Aho_Korasik.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Local/University-Github/6th-Sem-All/CPP/Aho-Korasik/Aho_Corasick.cpp" "D:/Local/University-Github/6th-Sem-All/CPP/Aho-Korasik/cmake-build-debug/CMakeFiles/Aho_Korasik.dir/Aho_Corasick.cpp.obj"
+  "D:/Local/University-Github/6th-Sem-All/CPP/Aho-Korasik/main.cpp" "D:/Local/University-Github/6th-Sem-All/CPP/Aho-Korasik/cmake-build-debug/CMakeFiles/Aho_Korasik.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Konstantinov/Aho-Korasik/cmake-build-debug/CMakeFiles/Aho_Korasik.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Aho_Korasik.exe"
+  "Aho_Korasik.exe.manifest"
+  "Aho_Korasik.pdb"
+  "CMakeFiles/Aho_Korasik.dir/Aho_Corasick.cpp.obj"
+  "CMakeFiles/Aho_Korasik.dir/main.cpp.obj"
+  "libAho_Korasik.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aho_Korasik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
