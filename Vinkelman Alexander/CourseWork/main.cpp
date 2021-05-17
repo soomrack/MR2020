527312e1e4b38577dd9f5e250aa651a401c3878a
AlexanderVinkelman <70704315+AlexanderVinkelman@users.noreply.github.com>
2021-05-17 12:07:59 +0300

    Add files via upload

---
+++ Vinkelman Alexander/CourseWork/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+#include <iterator>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef vector <int> Mass;
+
+int main() {
+    unsigned int a = 0, b = 0;
+    int Amount = 0;
+
+    // Создание итераторов
+    Mass::iterator LagA;
+    Mass::iterator LagB;
+    Mass::iterator Lag;
+
+    // Запрос у пользователя на ввод параметров
+    cout << "Lag a = ";
+    cin >> a;
+    cout << "Lag b = ";
+    cin >> b;
+    cout << "Amount of numbers ";
+    cin >> Amount;
+
+    // Создание массива
+    Mass massive(1);
+    massive.resize(max(a,b) + Amount + 1);
+
+    // Инициализация лагов и сдвиг
+    // на значения a и b
+    LagA = massive.begin();
+    advance(LagA, a);
+    LagB = massive.begin();
+    advance(LagB, b);
+
+    // Инициализация дополнительного итератора
+    // для коорректной работы цикла
+    if (LagA > LagB) {
+        Lag = LagA;
+    } else {
+        Lag = LagB;
+    }
+
+    // Основной итератор для движения по массиву
+    Mass::iterator index;
+    index = massive.begin();
+
+    // Заполнение первой части массива
+    // случайными значениями
+    srand(time(nullptr));
+    while (index <= Lag) {
+        *index = rand();
+        cout << "Number = " << *index << endl;
+        index++;
+    }
+
+    // Получение последовательности методом
+    // Фибоначчи с запаздыванием
+    for (; index != massive.end(); index++, LagA++, LagB++) {
+        if (*LagA >= *LagB) {
+            *index = *LagA - *LagB;
+        } else {
+            *index = *LagB - *LagA;
+        }
+        cout << *index << endl;
+    }
+
+    return 0;
+}
