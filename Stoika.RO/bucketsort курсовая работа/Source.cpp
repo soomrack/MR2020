924d7f4acad8c88870e88f7b532669a64633959e
Stoykaro <70840340+Stoykaro@users.noreply.github.com>
2021-05-17 11:00:36 +0300

    Add files via upload

--- Stoika.RO/bucketsort курсовая работа/Source.cpp
+++ Stoika.RO/bucketsort курсовая работа/Source.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
-#include <algorithm>
-#include <vector> //used for the sake of simplicity
+#include <time.h> 
+#include <vector>
+
 using namespace std;
- 
- 
-void b_sort(int sarray[], int array_size) 
+
+void sort(int parray[], int array_size)
 {
     int rows = 10;
     int** arr = new int* [rows];
     for (int i = 0; i < rows; i++)
-    {
-        arr[i] = new int[array_size+1];
-    }
+        arr[i] = new int[array_size + 1];
     for (int x = 0; x < 10; x++) arr[x][array_size] = 0;
     // main loop for each digit position
-    for (int digit = 1; digit <= 1000000000; digit *= 10) 
+    for (int digit = 1; digit <= 1000000000; digit *= 10)
     {
         // array to bucket
-        for (int i = 0; i < array_size; i++) 
+        for (int i = 0; i < array_size; i++)
         {
             // get the digit 0-9
-            int dig = (sarray[i] / digit) % 10;
+            int dig = (parray[i] / digit) % 10;
             // add to bucket and increment count
-            arr[dig][arr[dig][array_size]++] = sarray[i];
+            arr[dig][arr[dig][array_size]++] = parray[i];
         }
         // bucket to array
         int idx = 0;
         for (int x = 0; x < 10; x++)
         {
             for (int y = 0; y < arr[x][array_size]; y++)
-                sarray[idx++] = arr[x][y];
+                parray[idx++] = arr[x][y];
             // reset the internal bucket counters
             arr[x][array_size] = 0;
         }
@@ -37,10 +35,36 @@ void b_sort(int sarray[], int array_size)
     delete arr;
 }
 
+
+void bucketsort(int origin_array[], int array_size)
+{
+    //divide array into ng=egative and positive
+    int* negative_array = new int[array_size];
+    int negative_array_counter = 0;
+    int* positive_array = new int[array_size];
+    int positive_array_counter = 0;
+    for (int i = 0; i < array_size; i++)
+    {
+        if (origin_array[i] >= 0)
+            positive_array[positive_array_counter++] = origin_array[i];
+        else
+            negative_array[negative_array_counter++] = -origin_array[i];
+    }
+    sort(negative_array, negative_array_counter);
+    for (int i = negative_array_counter - 1, j = 0; i >= 0; j++, i--)
+        origin_array[i] = -negative_array[j];
+    sort(positive_array, positive_array_counter);
+    for (int i = negative_array_counter, j = 0; i < array_size; j++, i++)
+        origin_array[i] = positive_array[j];
+}
+
 int main()
 {
-    int sarray[10] = { 14,17,2,2,211,80,5,4,9,52 };
-    b_sort(sarray,10);
-    int a = 10;
-    a++;
+    const int num = 20000;
+    int sarray[num];
+    clock_t start = clock();
+    bucketsort(sarray, num);
+    clock_t end = clock();
+    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("The time: %f seconds\n", seconds);
 }
